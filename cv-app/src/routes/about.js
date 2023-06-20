960855e76d262714262f50b1e36aca542e87fe5d
= <=>
2023-06-20 20:48:18 +0200

    feat: add cv

---
+++ cv-app/src/routes/about.js
@@ -0,0 +1,19 @@
+import "./about.css"
+import AnimatedPage from '../AnimatedPage';
+
+import React from 'react';
+function About(){
+    return(
+        <AnimatedPage>
+        <>
+        <div className="about_me">
+        <h1>Hi, I am Aleksandra!</h1>
+        <p>Frontend developer with React and Angular</p>
+        <button>Contact me</button>
+        </div>
+        </>
+        </AnimatedPage>
+    )
+}
+
+export default About;
---
+++ cv-app/src/routes/contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AnimatedPage from '../AnimatedPage';
+import "./contact.css"
+
+function Contact(){
+    return(
+        <AnimatedPage>
+        <>
+        <div className="contact_container">
+        <h1>Contact me:</h1><br />
+        <p>email@gmail.com</p>
+        <p>discord: alex7894</p>
+        </div>
+        </>
+        </AnimatedPage>
+    )
+}
+
+export default Contact;
